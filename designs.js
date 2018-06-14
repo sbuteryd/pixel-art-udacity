a9b318a1dca2cf8857f35d71d5ef4ad6b1b28c8e
Russell <sbuteryd>
2018-06-14 14:27:00 +0800

    add each line

--- designs.js
+++ designs.js
@@ -1,10 +1,17 @@
 // Select color input
 // Select size input
-
+//
 // When size is submitted by the user, call makeGrid()
-//
-// function makeGrid() {
-//
-// // Your code goes here!
-//
-// }
+
+function makeGrid() {
+
+// Your code goes here!
+    const newTable = document.querySelector('#pixelCanvas');
+    for (let i = 0; i < 10; i++) {
+        const newTr = document.createElement('tr');
+        const sum = newTable.appendChild(newTr);
+    }
+
+
+}
+console.log(makeGrid());
