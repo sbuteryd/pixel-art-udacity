95d507fae54f911c0b23fe26f0f4eac51292544c
Russell <sbuteryd>
2018-06-14 14:28:21 +0800

    add each columns

--- designs.js
+++ designs.js
@@ -11,7 +11,12 @@ function makeGrid() {
         const newTr = document.createElement('tr');
         const sum = newTable.appendChild(newTr);
     }
-
-
+    const newFind = document.querySelector('#pixelCanvas').querySelectorAll('tr');
+    for (let i =0 ;i<10;i++){
+        for(let x=0;x<10;x++){
+            const newTd = document.createElement('td');
+            newFind[i].appendChild(newTd);
+        }
+    }
 }
 console.log(makeGrid());
