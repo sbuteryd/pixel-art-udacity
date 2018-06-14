6ed7df3b20e7cf67f62c71695f5a13b528ef59ed
Russell <sbuteryd>
2018-06-14 14:42:45 +0800

    dynamic user input

--- designs.js
+++ designs.js
@@ -3,20 +3,20 @@
 //
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+function makeGrid(height,width) {
 
 // Your code goes here!
     const newTable = document.querySelector('#pixelCanvas');
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < height; i++) {
         const newTr = document.createElement('tr');
         const sum = newTable.appendChild(newTr);
     }
     const newFind = document.querySelector('#pixelCanvas').querySelectorAll('tr');
-    for (let i =0 ;i<10;i++){
-        for(let x=0;x<10;x++){
+    for (let i =0 ;i<width;i++){
+        for(let x=0;x<width;x++){
             const newTd = document.createElement('td');
             newFind[i].appendChild(newTd);
         }
     }
 }
-console.log(makeGrid());
+console.log(makeGrid(2,3));
